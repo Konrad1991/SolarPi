4883f989e2e147e22b2a15c2764f764befb9abb6
Konrad1991 <konrad_kraemer@yahoo.de>
2024-03-05 13:17:20 +0100

    removed typo in main.rs

--- solar_pi/src/main.rs
+++ solar_pi/src/main.rs
@@ -1,7 +1,7 @@
 mod logging;
 
 fn main() {
-    logging::log_message("Starting SolarPi project...");
+    logging::log_message("Starting SolarPi project.");
     logging::log_message("SolarPi project finished.");
 }
 
