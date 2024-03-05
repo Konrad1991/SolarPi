1d7087424505676e03e3b87c238eb203722df4c2
Konrad1991 <konrad_kraemer@yahoo.de>
2024-03-05 13:52:40 +0100

    added some functionalities to logging

--- solar_pi/src/main.rs
+++ solar_pi/src/main.rs
@@ -1,8 +1,8 @@
 mod logging;
 
 fn main() {
-    logging::log_message("Starting SolarPi project.");
-    logging::log_message("SolarPi project finished.");
+    logging::log("Starting SolarPi project.");
+    logging::log("SolarPi project finished.");
 }
 
 
