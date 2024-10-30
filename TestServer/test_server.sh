867cf3b18e537d98fedab5cd732a473f7ea95e23
Konrad1991 <konrad_kraemer@yahoo.de>
2024-10-30 15:44:59 +0100

    removed TestClient TestServer

--- TestServer/test_server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# post file to route /CreateFile
-# 	file_name := c.Request.FormValue("FileName")
-# 	file, _, err := c.Request.FormFile("file")
-curl -X POST -F "file=@TestFile.txt" -F "FileName=DesiredFileName" http://localhost:8080/UploadFile
