c0e58f0eda8e35fc4485d81145d6742a669d245f
Konrad1991 <konrad_kraemer@yahoo.de>
2024-11-16 11:21:07 +0100

    Moved code from main in Server.go

--- cmd/main.go
+++ cmd/main.go
@@ -2,30 +2,19 @@ package main
 
 import (
 	"SolarPi/internal/Server"
+	"fmt"
 
-	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 // TODO: move code from func main in Server.go
 // TODO: create gin router with ip address as string.
-//        --> check within this function that string is a valid ip address
 // TODO: create Server_test.go file
 // TODO: write test for create router
 
 func main() {
-	err, router := Server.Server("IP address")
-	Server.DB, err = gorm.Open("sqlite3", "./internal/Server/test.db")
+	err := Server.StartServer(":8080")
 	if err != nil {
-		panic("Failed to connect to database")
-	}
-
-	Server.DB.AutoMigrate(&Server.File{})
-	defer Server.DB.Close()
-	if err != nil {
-		panic(err)
-	}
-	router.Run(":8080")
-	for {
+		fmt.Println(err)
 	}
 }
